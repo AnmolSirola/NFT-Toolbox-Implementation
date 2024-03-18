dbac3049f28bfc6caaaa5b40c05eb59fda85f634
Anmol <anmolsirola57@gmail.com>
2024-03-18 17:34:42 +0530

    Added functions to deploy the NFT Linker and ERC721 Demo contracts on each supported chain

--- Multi-Chain Support/Cross-Chain/index.ts
+++ Multi-Chain Support/Cross-Chain/index.ts
@@ -81,4 +81,78 @@ async function execute(chains: { source: ChainDetails; destination: ChainDetails
             tokenId: BigInt(tokenId),
         };
     };
+    
+    async function print(tokenId: number) {
+        const owner = await getOwnerDetails();
+        console.log(`Token '${tokenId}' was originally minted at ${source.name} is at ${owner.chain}.`);
+    }
+
+    const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    console.log('==== Initially ====');
+    await source.erc721.mintWithMetadata(tokenId, hash, metadata);
+    console.log(`Minted token ID: '${tokenId}' for ${source.name}`);
+
+    await print(tokenId);
+
+    const fee = await calculateBridgeFee(source, destination);
+
+    const owner = await getOwnerDetails();
+    const operatorAddress = source.erc721.address;
+
+    // Approve NFT to the NFTLinker contract
+    console.log(`\n==== Approve Original NFT to NFTLinker's contract if needed ====`);
+    const srcApproveTx = await source.erc721.approve(source.contract.address, owner.tokenId);
+    console.log(`Approved Original NFT (${tokenId}) to NFTLinker's contract`);
+    console.log(`Tx Approved Hash: ${srcApproveTx.hash}`);
+
+    // Send NFT to NFTLinker's contract
+    console.log("\n==== Send NFT to NFTLinker's contract ====");
+    const sendNftTx = await source.contract.sendNFT(operatorAddress, owner.tokenId, destination.name, wallet.address, { value: fee });
+    console.log(`Sent NFT ${tokenId} to NFTLinker's contract`, sendNftTx.hash);
+
+    const destinationTokenId = BigInt(
+        keccak256(defaultAbiCoder.encode(['string', 'address', 'uint256', 'string'], [source.name, operatorAddress, tokenId, metadata])),
+    );
+    console.log(`Token ID at ${destination.name} will be: '${destinationTokenId}'`);
+
+    while (true) {
+        const originalDetails = await destination.contract.original(destinationTokenId);
+        if (originalDetails !== '0x') break;
+        await sleep(2000);
+    }
+
+    console.log('\n==== Verify Result ====');
+    await print(destinationTokenId);
+
+    console.log("\n==== Approve Remote NFT to NFTLinker's contract ====");
+    const destApproveTx = await destination.contract.approve(destination.contract.address, destinationTokenId);
+    console.log(`Approved Remote NFT (${destinationTokenId}) to NFTLinker's contract`);
+    console.log(`Tx Approved Hash: ${destApproveTx.hash}`);
+
+    console.log(`\n==== Send NFT back from '${source.name}' to ${destination.name} ====`);
+    const sendBackNftTx = await destination.contract.sendNFT(
+        destination.contract.address,
+        destinationTokenId,
+        source.name,
+        wallet.address,
+        {
+            value: fee,
+        },
+    );
+    console.log(`Sent NFT ${destinationTokenId} back to ${source.name}`, sendBackNftTx.hash);
+
+    while (true) {
+        const owner = await source.erc721.ownerOf(tokenId);
+        if (owner === wallet.address) break;
+        await sleep(2000);
+    }
+
+    console.log('\n==== Verify Result ====');
+    await print(tokenId);
 }
+
+export {
+    deploy,
+    execute,
+};
