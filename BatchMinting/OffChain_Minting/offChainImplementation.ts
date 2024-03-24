3c673a670a142687342e0e7f1c8f1c4e5ffa5b6c
Anmol <anmolsirola57@gmail.com>
2024-03-24 11:30:52 +0530

    Updated offChain Implementation

--- BatchMinting/OffChain_Minting/offChainImplementation.ts
+++ BatchMinting/OffChain_Minting/offChainImplementation.ts
@@ -1,34 +1,27 @@
-const Web3 = require('web3')
-const MyNFT = require('./MyNFT.json')
+import Web3 from 'web3';
+import { AbiItem } from 'web3-utils';
+import MyNFT from './MyNFT.json';
 
-const web3 = new Web3('http://localhost:8545') // replace this URL with the Ethereum node URL
+const web3 = new Web3('http://localhost:8545'); // replace this URL with the Ethereum node URL
+
 const myNFTContract = new web3.eth.Contract(
-  MyNFT.abi,
-  'With out abi'
-)
+  MyNFT.abi as AbiItem[],
+  'Contract ABI'
+);
 
-async function offChain(recipients, tokenIds) {
-  const batchSize = 10 // number of NFTs to mint in each transaction, let it be 10 here.
-  const numBatches = Math.ceil(
-    recipients.length / batchSize
-  )
+async function offChain(recipients: string[], tokenIds: number[]) {
+  const batchSize = 10; // number of NFTs to mint in each transaction, let it be 10 here.
+  const numBatches = Math.ceil(recipients.length / batchSize);
+
   for (let i = 0; i < numBatches; i++) {
-    const start = i * batchSize
-    const end = Math.min(
-      (i + 1) * batchSize,
-      recipients.length
-    )
-    const batchRecipients = recipients.slice(
-      start,
-      end
-    )
-    const batchTokenIds = tokenIds.slice(
-      start,
-      end
-    )
+    const start = i * batchSize;
+    const end = Math.min((i + 1) * batchSize, recipients.length);
+    const batchRecipients = recipients.slice(start, end);
+    const batchTokenIds = tokenIds.slice(start, end);
+
     await myNFTContract.methods
       .batchMint(batchRecipients, batchTokenIds)
-      .send({ from: '<Our Ethereum address>' })
+      .send({ from: '<Our Ethereum address>' });
   }
 }
 
@@ -37,6 +30,8 @@ const recipients = [
   'Address 1 of recipient',
   'Address 2 of recipient',
   'Address 3 of recipient',
-]
-const tokenIds = [0, 1, 2]
-await batchMint(recipients, tokenIds)
+];
+
+const tokenIds = [0, 1, 2];
+
+offChain(recipients, tokenIds);
